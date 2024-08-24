ff12671051fd49d291621ab544b28ad4ed33a464
Priyanshu Singh <priyanshusingh0610@gmail.com>
2024-08-24 19:37:34 +0530

    Refactored code in UserService and optimized the code which will alo improve performance

--- Authentication/src/main/java/com/cryp73r/authentication/service/UserService.java
+++ Authentication/src/main/java/com/cryp73r/authentication/service/UserService.java
@@ -1,134 +1,88 @@
 package com.cryp73r.authentication.service;
 
-import com.cryp73r.authentication.exception.UnknownUserException;
-import com.cryp73r.authentication.exception.UserNotFoundException;
-import com.cryp73r.authentication.model.Session;
+import com.cryp73r.authentication.exception.BadCredentialsException;
+import com.cryp73r.authentication.exception.InternalServerErrorException;
+import com.cryp73r.authentication.mapper.RoleMapper;
+import com.cryp73r.authentication.mapper.UserMapper;
 import com.cryp73r.authentication.model.User;
-import com.cryp73r.authentication.repository.SessionRepository;
 import com.cryp73r.authentication.repository.UserRepository;
-import com.cryp73r.authentication.sdo.Status;
-import com.cryp73r.authentication.sdo.UserSDO;
-import com.cryp73r.authentication.sdo.VoidReturnTypeResponseSDO;
-import com.cryp73r.authentication.security.IdentifierManager;
-import com.cryp73r.authentication.security.PasswordManager;
-import com.cryp73r.authentication.security.SessionManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.cryp73r.authentication.dto.UserDTO;
+import com.cryp73r.authentication.security.jwt.JwtUtils;
+import com.cryp73r.authentication.security.service.ClientUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private SessionRepository sessionRepository;
+    private final UserMapper userMapper;
 
-    public UserService() {
+    private final RoleService roleService;
 
-    }
-    public UserSDO createUser(User user) {
-        PasswordManager passwordManager = new PasswordManager();
-        IdentifierManager identifierManager = new IdentifierManager();
-        SessionManager sessionManager = new SessionManager();
-        String encodedPassword = passwordManager.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        String identifier = identifierManager.generateIdentifier();
-        user.setIdentifier(identifier);
-        String token = sessionManager.generateToken(identifier);
-        Session session = new Session(token, user);
-        List<Session> sessionList = new ArrayList<>();
-        sessionList.add(session);
-        user.setSessionList(sessionList);
-        userRepository.save(user);
-        Status status = new Status(HttpStatus.CREATED, user.getUsername() + " is created successfully");
-        return new UserSDO(status, user, token);
+    private final ClientUserDetailsService clientUserDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    final
+    JwtUtils jwtUtils;
+    private final RoleMapper roleMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper, RoleService roleService, ClientUserDetailsService clientUserDetailsService, PasswordEncoder passwordEncoder, JwtUtils jwtUtils, RoleMapper roleMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.roleService = roleService;
+        this.clientUserDetailsService = clientUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.roleMapper = roleMapper;
     }
 
-    public UserSDO loginUser(User user) {
-        User userWithUsername = null;
-        for (User iterUser : userRepository.findAll()) {
-            if (iterUser.getUsername().equals(user.getUsername())) {
-                userWithUsername = iterUser;
-                break;
+    @Transactional
+    public String signUp(UserDTO userDTO) {
+        User user = userMapper.UserDTOToUser(userDTO);
+        if (!userDTO.getRoleNames().isEmpty()) {
+            for (String roleName : userDTO.getRoleNames()) {
+                user.addRole(roleMapper.RoleDTOToRole(roleService.getRole(roleName)));
             }
+        } else {
+            user.addRole(roleMapper.RoleDTOToRole(roleService.getRole("GENERAL_USER")));
         }
-        if (userWithUsername == null) {
-            throw new UserNotFoundException(user.getUsername() + " doesn't exist, please check the username once");
+        try {
+            userRepository.save(user);
+            UserDetails userDetails = clientUserDetailsService.loadUserByUsername(user.getEmail());
+            return "Bearer " + jwtUtils.generateTokenFromUsername(userDetails);
+        } catch (Exception e) {
+            throw new InternalServerErrorException("Error encountered while saving or generating token");
         }
-        PasswordManager passwordManager = new PasswordManager();
-        boolean isValid = passwordManager.matches(user.getPassword(), userWithUsername.getPassword());
-        if (isValid) {
-            SessionManager sessionManager = new SessionManager();
-            String token = sessionManager.generateToken(userWithUsername.getIdentifier());
-            Session session = new Session(token, userWithUsername);
-            List<Session> sessionList = new ArrayList<>();
-            sessionList.add(session);
-            userWithUsername.setSessionList(sessionList);
-            userRepository.save(userWithUsername);
-            Status status = new Status(HttpStatus.OK, userWithUsername.getUsername() + " logged in successfully");
-            return new UserSDO(status, userWithUsername, token);
-        } else throw new UnknownUserException("Invalid username/password");
     }
 
-    public UserSDO getUser(String token) {
-        User userToGet = validateTokenAndFindUser(token);
-        if (userToGet == null) throw new UserNotFoundException("User doesn't exist, please check the authorization once");
-        Status status = new Status(HttpStatus.OK, userToGet.getUsername()+"'s profile fetched successfully");
-        return new UserSDO(status, userToGet);
-    }
-
-    public VoidReturnTypeResponseSDO logoutUser(String token) {
-        User userToLogout = validateTokenAndFindUser(token);
-        if (userToLogout == null) {
-            Status status = new Status(HttpStatus.UNAUTHORIZED, "Invalid authentication token or the token is expired");
-            return new VoidReturnTypeResponseSDO(status);
-        }
-        Session sessionToRemove = null;
-        for (Session session : userToLogout.getSessionList()) {
-            if (session.getToken().equals(token)) sessionToRemove = session;
-        }
-        List<Session> updatedSessionList = userToLogout.getSessionList();
-        updatedSessionList.remove(sessionToRemove);
-        userToLogout.setSessionList(updatedSessionList);
-        userRepository.save(userToLogout);
-        assert sessionToRemove != null;
-        sessionRepository.delete(sessionToRemove);
-        Status status = new Status(HttpStatus.OK, userToLogout.getUsername() + " is logged out successfully");
-        return new VoidReturnTypeResponseSDO(status);
-    }
-
-    public VoidReturnTypeResponseSDO deleteUser(String token) {
-        User userToDelete = validateTokenAndFindUser(token);
-        if (userToDelete == null) {
-            Status status = new Status(HttpStatus.UNAUTHORIZED, "Invalid authentication token or the token is expired");
-            return new VoidReturnTypeResponseSDO(status);
-        }
-        userRepository.delete(userToDelete);
-        Status status = new Status(HttpStatus.OK, userToDelete.getUsername() + " is deleted successfully");
-        return new VoidReturnTypeResponseSDO(status);
-    }
-
-    private User validateTokenAndFindUser(String token) {
-        SessionManager sessionManager = new SessionManager();
-        boolean isExpired = sessionManager.isTokenExpired(token);
-        if (isExpired) return null;
-        String identifier = sessionManager.getIdentifier(token);
-        User userToReturn = null;
-        for (User user : userRepository.findAll()) {
-            if (user.getIdentifier().equals(identifier)) {
-                userToReturn = user;
-                break;
+    @Transactional
+    public String loginUser(UserDTO userDTO) {
+        try {
+            UserDetails userDetails = clientUserDetailsService.loadUserByUsername(userDTO.getEmail());
+            if (passwordEncoder.matches(userDTO.getPassword(), userDetails.getPassword())) {
+                return "Bearer " + jwtUtils.generateTokenFromUsername(userDetails);
+            } else {
+                throw new BadCredentialsException("Invalid username or password");
             }
+        } catch (UsernameNotFoundException e) {
+            throw new BadCredentialsException("Invalid username or password");
         }
-        if (userToReturn == null) return null;
-        for (Session session : userToReturn.getSessionList()) {
-            if (session.getToken().equals(token)) return userToReturn;
-        }
-        return null;
+    }
+
+    public UserDTO getCurrentUser(String authenticatedUsername) {
+        return userMapper.UserToUserDTO(userRepository.findByIdentifier(authenticatedUsername).orElseThrow(() -> new BadCredentialsException("User not found")));
+    }
+
+    @Transactional
+    public void deleteUser(String authenticatedUsername) {
+        User user = userRepository.findByIdentifier(authenticatedUsername).orElseThrow(() -> new BadCredentialsException("User not found"));
+        user.removeAllRoles();
+        userRepository.delete(user);
     }
 }

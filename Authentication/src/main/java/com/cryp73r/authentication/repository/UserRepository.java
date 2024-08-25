6290f73f2691df2902a22be08193865fee81617a
Priyanshu Singh <priyanshusingh0610@gmail.com>
2024-08-26 02:43:12 +0530

    Deprecated getByIdetifier and introdcued getByEmail to fetch userDetails from DB

--- Authentication/src/main/java/com/cryp73r/authentication/repository/UserRepository.java
+++ Authentication/src/main/java/com/cryp73r/authentication/repository/UserRepository.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Deprecated
     @Query("SELECT usr FROM User usr WHERE usr.identifier = :identifier")
     Optional<User> findByIdentifier(String identifier);
+
+    @Query("SELECT usr FROM User usr WHERE usr.email = :email")
+    Optional<User> findByEmail(String email);
 }
